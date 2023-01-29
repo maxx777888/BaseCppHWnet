2d212cbafa1dbf5f868964e957d4a83b1804a99c
Andrei <test.email.com>
2023-01-29 15:05:26 +0100

    first commit

---
+++ classHWcalculator3.1/classHWcalculator3.1.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+
+class Calculator
+{
+public:  
+    //Метод сложения
+    double add() {
+        return num1 + num2;
+    }
+    //Метод вычитания из второго числа первого
+    double subtract_1_2() {
+        return num2 - num1;
+    }
+    //Метод вычитания из первого числа второго
+    double subtract_2_1() {
+        return num1 - num2;
+    }
+    //Метод умножения
+    double multiply() {
+        return num1 * num2;
+    }
+    //Метод деления первого числа на второе
+    double divide_1_2() {
+        return num1 / num2;
+    }
+    //Метод деления вторго числа на первое
+    double divide_2_1() {
+        return num2 / num1;
+    }
+    bool set_num1(double num1); //Присваивание значение первому числу
+    bool set_num2(double num2); //Присваивание значение второму числу
+
+private:
+    double num1;
+    double num2;
+};
+
+bool Calculator::set_num1(double num1) {
+    if (num1 != 0) {
+        this->num1 = num1;
+        return true;
+    }
+    else {
+        std::cout << "Неверный ввод!" << std::endl;
+        return false;
+    }
+}
+
+bool Calculator::set_num2(double num2) 
+{
+    if (num2 != 0) {
+        this->num2 = num2;
+        return true;
+    }
+    else {
+        std::cout << "Неверный ввод!" << std::endl;
+        return false;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    do
+    {
+        double n1, n2;
+        Calculator numbers;
+        bool check = false;
+        //Запрос и проверка на 0 первого числа 
+        std::cout << "Введите num1:\n";
+        std::cin >> n1;
+        check = numbers.set_num1(n1);
+        while (check != true) {
+            std::cout << "Введите num1:\n";
+            std::cin >> n1;
+            check = numbers.set_num1(n1);
+        }
+        //Запрос и проверка на 0 второго числа 
+        std::cout << "Введите num2:\n";
+        std::cin >> n2;
+        check = numbers.set_num2(n2);
+        while (check != true) {
+            std::cout << "Введите num2:\n";
+            std::cin >> n2;
+            check = numbers.set_num2(n2);
+        }
+        //Вывод всех операций с числами на экран
+        std::cout << "num1 + num2 = " << numbers.add() << std::endl;
+        std::cout << "num1 - num2 = " << numbers.subtract_2_1() << std::endl;
+        std::cout << "num2 - num = " << numbers.subtract_1_2() << std::endl;
+        std::cout << "num1 * num = " << numbers.multiply() << std::endl;
+        std::cout << "num1 / num2 = " << numbers.divide_1_2() << std::endl;
+        std::cout << "num2 / num1 = " << numbers.divide_2_1() << std::endl;
+
+    } while (true);
+    
+
+    
+    
+
+
+}
+
---
+++ classHWcounter3.2/classHWcounter3.2.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+class Counter
+{
+public:
+	//Конструктор без параметров
+    Counter (){
+        count = 1;
+    }
+	//Конструктор с параметрами заданными пользователем
+    Counter (int _count){
+        count = _count;
+    }
+	//Метод вывода на экран текущего состояния счетчика
+    void print_count() {
+        std::cout << "Значение счетчика: " << count << std::endl;
+    }
+	//Метод добавление к счетчику единицы
+    void add() {
+        count += 1;
+    }
+	//Метод вычетания из счетчика единицы
+    void sub() {
+		count -= 1;    
+    }
+private:
+	int count;
+    
+};
+
+//Проверка на изначальный выбор пользователя для инициализации счетчика
+bool is_YES(std::string str) 
+{
+	if ((str == "YES") || (str == "Yes") || (str == "yes") || (str == "ДА") || (str == "да") || (str == "Да")) {
+		return true;
+	}
+	else {
+		return false;
+	}
+	
+ }
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+	
+    std::string str;
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите YES(ДА) или NO(Любой символ):\n";
+    std::cin >> str;
+    char x; 
+    bool exit = false; //Переменная которая отвечает за выход из цикла
+
+    //Проверка на инициализацию счетчика пользователем
+	if (is_YES(str)) {
+		int num;
+		std::cout << "Введите начальное значение счётчика: " << std::endl;
+		std::cin >> num;
+		Counter counter(num);//Установка счетчика данными полученными от пользователя
+
+		do
+		{
+			//Операции со счетчиком взависимости от ввода символов пользователем
+			std::cout << "Введите команду ('+', '-', '=' или 'x'):" << std::endl;
+			std::cin >> x;
+
+			if (x == '+') { //Добавление в счетчик
+				counter.add();
+			}
+			else if (x == '-') { //Вычитание из счетчика
+				counter.sub();
+			}
+			else if (x == '=') { //Вывод значения счетчика на экран
+				counter.print_count();
+			}
+			else if ((x == 'x') || (x == 'х')) { //Выход из цикла
+				exit = true;
+			}
+		} while (exit != true);
+
+	}
+	else {
+		Counter counter; //Установка счетчика конструктором без параметров.
+		do
+		{
+			//Операции со счетчиком взависимости от ввода символов пользователем
+			std::cout << "Введите команду ('+', '-', '=' или 'x'):" << std::endl;
+			std::cin >> x;
+
+			if (x == '+') { //Добавление в счетчик
+				counter.add();
+			}
+			else if (x == '-') { //Вычитание из счетчика
+				counter.sub();
+			}
+			else if (x == '=') { //Вывод значения счетчика на экран
+				counter.print_count();
+			}
+			else if ( (x == 'x') || (x == 'х')) { //Выход из цикла
+				exit = true;
+			}
+		} while (exit != true);
+	}
+	std::cout << "До свидания!" << std::endl;
+
+}
+
